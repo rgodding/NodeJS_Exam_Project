9dcbea8e35d6b18de14eb661ef07268d89f48349
Shaundator <rash0081@stud.kea.dk>
2023-05-25 07:34:57 +0200

    cleaned some code

--- client/app.js
+++ client/app.js
@@ -1,43 +1,34 @@
-import express from "express";
+import express from 'express';
 import session from 'express-session';
-import dotenv from "dotenv/config";
+import dotenv from 'dotenv/config';
 
 const app = express();
-app.use(express.static("public"));
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 
 app.use(
-    session({
-      secret: 'secret-key',
-      resave: false,
-      saveUninitialized: true,
-    })
-  );
+  session({
+    secret: 'secret-key',
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 
-import http from "http";
-import { Server } from "socket.io";
+import http from 'http';
+import { Server } from 'socket.io';
 const server = http.createServer(app);
 export const io = new Server(server);
-import socketIo from "./middleware/socketIo.js";
-socketIo(io)
+import socketIo from './middleware/socketIo.js';
+socketIo(io);
 
-import frontpageRouter from "./routers/frontpageRouter.js"
-import loginRouter from "./routers/loginRouter.js"
-import userRouter from "./routers/userRouter.js"
-import adminRouter from "./routers/adminRouter.js"
-import documentsRouter from "./routers/documentsRouter.js"
-import imagesRouter from "./routers/imagesRouter.js"
-const routers = [
-    frontpageRouter,
-    loginRouter,
-    userRouter,
-    adminRouter,
-    documentsRouter,
-    imagesRouter,
-]
-
-
+import frontpageRouter from './routers/frontpageRouter.js';
+import loginRouter from './routers/loginRouter.js';
+import userRouter from './routers/userRouter.js';
+import adminRouter from './routers/adminRouter.js';
+import documentsRouter from './routers/documentsRouter.js';
+import imagesRouter from './routers/imagesRouter.js';
+const routers = [frontpageRouter, loginRouter, userRouter, adminRouter, documentsRouter, imagesRouter];
 
 app.use(routers);
 const PORT = process.env.PORT || 8075;
-server.listen(PORT, () => console.log("Server is running on port", PORT));
+server.listen(PORT, () => console.log('Server is running on port', PORT));
