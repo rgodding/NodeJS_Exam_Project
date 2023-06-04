a872ade2c547c000985ae9303b35190422661f75
Shaundator <rash0081@stud.kea.dk>
2023-06-04 12:33:19 +0200

    commit

--- client/controllers/frontpageController.js
+++ client/controllers/frontpageController.js
@@ -1,12 +1,8 @@
 import constructFrontpagePage from '../util/pages/constructFrontpagePage.js';
-import fs from "fs"
 export function showFrontpage(req, res) {
   try {
     const page = constructFrontpagePage(req.isUser, req.userId);
-    const image = fs.readFileSync('./uploads/0mk07wzuahny.jpg');
-    const imageBase64 = image.toString('base64');
-    const htmlWithImage = page.replace('<img src="placeholder">', `<img src="data:image/jpeg;base64,${imageBase64}">`);
-    res.send(htmlWithImage);
+    res.send(page);
   } catch (err) {
     console.error(err);
     res.status(505).send('Internal Client Error');
